a3f96efca4b3928a2c5e9d61d37a3f48315cba84
colt <necromanga@gmail.com>
2016-12-12 14:10:12 +0100

    Initial commit

---
+++ app/src/main/java/com/koltinjo/android179_camera2_api/ActivityMain.java
@@ -0,0 +1,15 @@
+package com.koltinjo.android179_camera2_api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// https://www.youtube.com/watch?v=CuvVpsFc77w&index=1&list=PL9jCwTXYWjDIHNEGtsRdCTk79I9-95TbJ
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
